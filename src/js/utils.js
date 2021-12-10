8c87507d17238e42d93555362ed0479ea142f806
AlexanderAndreevGIT <andreev-alex@inbox.ru>
2021-12-11 01:27:14 +0300

    initial commit

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,32 @@
 export function calcTileType(index, boardSize) {
-  // TODO: write logic here
+  const topCells = index < (boardSize - 1);
+  const leftCells = Number.isInteger(index / boardSize);
+  const rightCells = Number.isInteger((index + 1) / boardSize)
+  const bottomCells = index >= (boardSize ** 2 - boardSize)
+  if (index === 0) {
+    return 'top-left';
+  }
+  if (index === (boardSize - 1)) {
+    return 'top-right';
+  }
+  if (index === boardSize ** 2 - boardSize) {
+    return 'bottom-left';
+  }
+  if (index === boardSize ** 2 - 1) {
+    return 'bottom-right';
+  }
+  if (topCells) {
+    return 'top';
+  }
+  if (leftCells) {
+    return 'left';
+  }
+  if (rightCells) {
+    return 'right';
+  }
+  if (bottomCells) {
+    return 'bottom';
+  }
   return 'center';
 }
 
